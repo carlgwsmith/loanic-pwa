a58ed7c26a28b4f0c729b035483ef60dc7ba6a59
carlvskansas <48308402+carlgwsmith@users.noreply.github.com>
2021-11-30 23:04:38 -0600

    homepage loanic

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Header, Logo, Wordmark, Subtitle, WelcomeText, SectionBtn, NavMenu } from '../Shared/SharedElements';
 
 const Home = () => {
   return (
     <div>
-      Home TEST
+     <Header className="wave-container">
+     <Logo>
+        <Wordmark>
+          Loanic
+        </Wordmark>
+        <Subtitle>Mortgage Tools</Subtitle>
+      </Logo>
+     <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 350" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><path d="M 0,500 C 0,500 0,250 0,250 C 112.85714285714286,210.10714285714286 225.71428571428572,170.21428571428572 346,193 C 466.2857142857143,215.78571428571428 594,301.25 732,327 C 870,352.75 1018.2857142857144,318.78571428571433 1138,296 C 1257.7142857142856,273.21428571428567 1348.8571428571427,261.60714285714283 1440,250 C 1440,250 1440,500 1440,500 Z" stroke="none" stroke-width="0" fill="#00d084ff" class="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 250)"></path></svg>
+     </Header>
+     <WelcomeText>
+     How can we help your with your mortgage?
+     </WelcomeText>
+     <NavMenu>
+     <SectionBtn to="/">
+       <p style={{margin:'0px', lineHeight:'0', textAlign:'center'}}>Monthly Mortgage Payment</p>
+      </SectionBtn>
+     <SectionBtn to="/">Mortgage Payoff</SectionBtn>
+     <SectionBtn to="/">What can I afford?</SectionBtn>
+     <SectionBtn to="/">Homebuyers Glossary</SectionBtn>
+     </NavMenu>
     </div>
   );
 }
--- src/Shared/SharedElements.js
+++ src/Shared/SharedElements.js
@@ -1,5 +1,66 @@
 import styled from "styled-components";
+import { NavLink as Link } from 'react-router-dom';
 
-export const header = styled.div`
-background-color: green;
+export const Header = styled.div`
+/* background-image: url('../waves.svg');
+height:290px;
+background-repeat: no-repeat;
+background-size: auto;
+background-position:bottom;
+background-color:#00d084; */
 `
+export const Logo = styled.div`
+background-color:#00d084;
+height:100px;
+padding-top:20px;
+/* margin: 7rem 1.25rem 2.5rem 1.25rem; */
+`
+export const Wordmark = styled.p`
+font-family:'Poppins', sans-serif;
+font-size:4em;
+margin: 0 1.25rem .25rem 1.25rem;
+color:white;
+line-height: 1;
+`
+export const Subtitle = styled.p`
+font-family:'Poppins', sans-serif;
+margin:0px;
+color:white;
+font-weight: 400;
+letter-spacing: 3px;
+`
+export const WelcomeText = styled.p`
+font-size:1.2em;
+color:#6C6B6B;
+font-family:'Manrope', sans-serif;
+margin:0px 0px 20px 0px;
+`
+
+export const SectionBtn = styled(Link)`
+border: 3px solid #1F9D61;
+border-radius:14px;
+cursor: pointer;
+font-size:1em;
+font-weight: 500;;
+transition: 0.2s ease-in-out;
+display:flex;
+align-items: center;
+justify-content: center;
+margin: 0px 20px 20px 20px;
+padding:30px;
+&.active{
+  color:#1F9D61;
+  text-decoration: none;;
+}
+&:hover{
+  background-color: #1F9D61;
+  color:white;
+  transition: 0.2s ease-in-out;
+}
+`
+export const NavMenu = styled.div`
+display:grid;
+grid-template-columns: 1fr;
+grid-template-rows: repeat(6, 80px);
+text-align:center;
+  `
