7c2c6911ec840437511aa35873ca090515ed36bb
carlvskansas <48308402+carlgwsmith@users.noreply.github.com>
2021-11-30 21:34:03 -0600

    Loanic PWA Initialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import React, {useState} from 'react';
+import Home from './Pages/Home';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <Router basename={process.env.PUBLIC_URL}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" exact element={<Home />}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/Shared/SharedElements.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const header = styled.div`
+background-color: green;
+`
