12d871b15d51fedb698268d76071ec4439ec5f46
carlvskansas <48308402+carlgwsmith@users.noreply.github.com>
2021-12-02 23:05:54 -0600

    Update MonthlyPayment.js

--- src/Pages/MonthlyPayment.js
+++ src/Pages/MonthlyPayment.js
@@ -1,17 +1,42 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../Shared/Header';
 import Sidebar from '../Nav/Sidebar';
 
 const Monthlypayment = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const [homePrice, setHomePrice] = useState(0)
+  const [downPayment, setDownPayment] = useState(0)
+  const [loanLength, setLoanLength] = useState(0)
+  const [interestRate, setInterestRate] = useState(0)
 
   const toggle = () => {
     setIsOpen(!isOpen)
   }
+  const inputStyle={
+    padding:'8px',
+    fontSize:'20px',
+    marginBottom:'10px',
+    border:'1px solid #333 !important'
+  }
+  const label={
+    fontSize:'15px',
+    fontFamily:'"Poppins", sans-serif',
+    paddingTop:'10px',
+    margin:'0px'
+  }
+
   return (
     <div>
       <Header name="Monthly Payment Calculator" toggle={toggle}/>
       <Sidebar isOpen={isOpen} toggle={toggle}/>
+      <p style={label}>Home Price</p>
+      <input type="number" style={inputStyle} onChange={(val) => setHomePrice(val.target.value)}/>
+      <p  style={label}>Down Payment</p>
+      <input type="number" style={inputStyle} onChange={(val) => setDownPayment(val.target.value)}/>
+      <p  style={label}>Mortgage Length</p>
+      <input type="number" style={inputStyle} onChange={(val) => setLoanLength(val.target.value)}/>
+      <p  style={label}>Interest Rate</p>
+      <input type="number" style={inputStyle} onChange={(val) => setInterestRate(val.target.value)}/>
     </div>
   );
 }
